04e297df37d2bac701a5b8bb18856260dbf31309
我只想看看蓝天 <1207032539@qq.com>
2018-11-02 23:19:54 +0800

    加密算法优化

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 这是一个PHP数据加密解密包，可以设置密码、过期时间，在一些需要对数据进行加密解密传输的应用中非常有用
 # 使用例子
 ```
-$cleartext = "1111111111111li111111111123111111";
-$cipher = encrypt ( $cleartext, "123456");
+$crypt = new \Php\Crypt\Crypt();
+$cleartext = "需要加密的数据";
+$cipher = $crypt->encrypt($cleartext, "123456");
 echo '密文：' . $cipher . "\n";
-echo '明文：' . decrypt ( $cipher, "123456" ) . "\n";
+echo '明文：          ' . $crypt->decrypt($cipher, "123456") . "\n";
 //设置秘钥超时失效，只需把expires参数设置有效时间，单位是秒,比如有效期为一分钟，则设置expires为60
-$cipher = encrypt ( $cleartext, "123456",60);
-echo '密文：' . $cipher . "\n";
-echo '明文：' . decrypt ( $cipher, "123456" ) . "\n";
+$cipher = $crypt->encrypt($cleartext, "123456", 60);
+echo '密文：       ' . $cipher . "\n";
+echo '明文：          ' . $crypt->decrypt($cipher, "123456") . "\n";
 ```
--- src/Php/Crypt/Crypt.php
+++ src/Php/Crypt/Crypt.php
@@ -1,140 +1,124 @@
 <?php
+
 namespace Php\Crypt;
 
 class Crypt
 {
-	/**
-	 * 加密
-	 * @param unknown $data //要加密的数据
-	 * @param unknown $key  //加密秘钥
-	 * @param number $expires  //有效期
-	 * @return string   //返回加密过的数据
-	 */
-	public function encrypt($data, $key, $expires = 0) {
-		if (! is_int ( $expires ) || $expires < 0 || empty ( $key ) || empty ( $data )) {
-			throw new \Exception('There is a problem with the input parameters');
-		}
-		$setdate = '';
-		if ($expires >= 1) {
-			$setdate = '=';
-		}
-		$time = time () + $expires;
-		$data = base64_encode ( $time . $data );
-		// 加入时间参数
-		$key = strtoupper ( md5 ( $time . $key ) );
-		$arr = array_combine ( range ( 'A', 'Z' ), range ( 'Z', 'A' ) );
-		$pieces = str_split ( $data );
-		// 普通字符替换
-		$temp = 0;
-		// 加密核心
-		for($i = 0; $i < count ( $pieces ); $i ++) {
-			
-			foreach ( $arr as $k => $v ) {
-				if ($k == $pieces [$i]) {
-					$pieces [$i] = $v;
-					break;
-				}
-			}
-			
-			// 设置偏移
-			$pieces [$i] = ord ( $pieces [$i] ) + 1;
-			
-			// 与秘钥异或运算
-			$pieces [$i] = chr ( $pieces [$i] ^ ord ( $key [$temp] ) );
-			
-			if ((strlen ( $key ) - 1) <= $temp) {
-				$temp = 0;
-			} else {
-				$temp ++;
-			}
-		}
-		// 混淆时间
-		$arr2 = array (
-				'F',
-				'E',
-				'M',
-				'A',
-				'Q',
-				'J',
-				'Z',
-				'T',
-				'S',
-				'L'
-		);
-		$time = strval ( $time );
-		for($i = 0; $i < strlen ( $time ); $i ++) {
-			$time [$i] = $arr2 [$time [$i]];
-		}
-		return base64_encode ( $setdate . $time . implode ( '', $pieces ) );
-	}
-	/**
-	 * 解密
-	 * @param unknown $data  //已加密的数据
-	 * @param unknown $key   //解密秘钥
-	 * @return string|mixed  //返回解密结果
-	 */
-	public function decrypt($data, $key) {
-		if (empty ( $data ) || empty ( $key )) {
-			return 'data or key empty';
-		}
-		$data = base64_decode ( $data );
-		$checkdate = 0;
-		$nowtime=time();
-		if (substr ( $data, 0, 1 ) == '=') {
-			$checkdate = 1;
-			$time = substr ( $data, 1, 10 );
-			$data = substr ( $data, 11 );
-		} else {
-			$time = substr ( $data, 0, 10 );
-			$data = substr ( $data, 10 );
-		}
-		$arr2 = array (
-				'F',
-				'E',
-				'M',
-				'A',
-				'Q',
-				'J',
-				'Z',
-				'T',
-				'S',
-				'L'
-		);
-		$str = join ( $arr2 );
-		for($i = 0; $i < 10; $i ++) {
-			$time [$i] = strpos ( $str, $time [$i] );
-		}
-		if ($checkdate==1&&$time < $nowtime) {
-			throw new \Exception('The secret key expired');
-		}
-		$key = strtoupper ( md5 ( $time . $key ) );
-		$arr = array_combine ( range ( 'Z', 'A' ), range ( 'A', 'Z' ) );
-		$pieces = str_split ( $data );
-		$temp = 0;
-		for($i = 0; $i < count ( $pieces ); $i ++) {
-			$pieces [$i] = ord ( $pieces [$i] );
-			$pieces [$i] = $pieces [$i] ^ ord ( $key [$temp] );
-			
-			if ((strlen ( $key ) - 1) <= $temp) {
-				$temp = 0;
-			} else {
-				$temp ++;
-			}
-			$pieces [$i] = chr ( $pieces [$i] - 1 );
-			foreach ( $arr as $k => $v ) {
-				if ($k == $pieces [$i]) {
-					$pieces [$i] = $v;
-					break;
-				}
-			}
-		}
-		$result = base64_decode ( join ( '', $pieces ) );
-		if ($checkdate) {
-			if (substr ( $result, 0, 10 ) < $nowtime || substr ( $result, 0, 10 ) != $time) {
-				throw new \Exception('The secret key timeout');
-			}
-		}
-		
-		return str_replace ( $time, '', $result);
-	}
+    /**
+     * 加密
+     * @param unknown $data //要加密的数据
+     * @param unknown $key //加密秘钥
+     * @param number $expires //有效期
+     * @return string   //返回加密过的数据
+     */
+    public function encrypt($data, $key, $expires = 0)
+    {
+        if (!is_int($expires) || $expires < 0 || empty ($key) || empty ($data)) {
+            throw new \Exception('There is a problem with the input parameters');
+        }
+        //设置是否检测超时
+        if ($expires >= 1) {
+            $setdate = '*';
+        } else {
+            $setdate = '!';
+        }
+        $time = time() + $expires;
+        $data = base64_encode($time . $data);
+        // 加入时间参数
+        $key = strtoupper(md5($time . $key));
+        $arr = array_combine(range('A', 'Z'), range('Z', 'A'));
+        $pieces = str_split($data);
+        // 普通字符替换
+        $temp = 0;
+        // 加密核心
+        for ($i = 0; $i < count($pieces); $i++) {
+
+            //字符替换
+            $pieces[$i] = isset($arr[$pieces[$i]]) ? $arr[$pieces[$i]] : $pieces[$i];
+
+            // 设置偏移
+            $pieces [$i] = ord($pieces [$i]) + 1;
+
+            // 与秘钥异或运算
+            $pieces [$i] = chr($pieces [$i] ^ ord($key [$temp]));
+
+            if ((strlen($key) - 1) <= $temp) {
+                $temp = 0;
+            } else {
+                $temp++;
+            }
+        }
+        // 混淆时间
+        $arr2 = array('F', 'E', 'M', 'A', 'Q', 'J', 'Z', 'T', 'S', 'L');
+        $time = strval($time);
+        for ($i = 0; $i < strlen($time); $i++) {
+            $time [$i] = $arr2 [$time [$i]];
+        }
+        return base64_encode($setdate . $time . $setdate . implode('', $pieces));
+    }
+
+    /**
+     * 解密
+     * @param unknown $data //已加密的数据
+     * @param unknown $key //解密秘钥
+     * @return string|mixed  //返回解密结果
+     */
+    public function decrypt($data, $key)
+    {
+        if (empty ($data) || empty ($key)) {
+            return 'data or key empty';
+        }
+        $data = base64_decode($data);
+        $checkdate = 0;
+        $nowtime = time();
+        if (substr($data, 0, 1) == '!') {
+            $checkdate = 1;
+            $needle = '!';
+        } else {
+            $needle = '*';
+        }
+        $time = substr($data, strpos($data, $needle, 0) + 1, strpos($data, $needle, 1) - 1);
+        $data = substr($data, strpos($data, $needle, 1) + 1);
+        //判断加密数据合法性
+        if (empty($time) || empty($data)) {
+            throw new \Exception('the encrypted data is invalid');
+        }
+        unset($data_explodes);
+        //恢复时间
+        $arr2 = array('F', 'E', 'M', 'A', 'Q', 'J', 'Z', 'T', 'S', 'L');
+        $str = join($arr2);
+        for ($i = 0; $i < 10; $i++) {
+            $time [$i] = strpos($str, $time [$i]);
+        }
+        if ($checkdate == 1 && $time < $nowtime) {
+            throw new \Exception('The secret key expired');
+        }
+        $key = strtoupper(md5($time . $key));
+        $arr = array_combine(range('Z', 'A'), range('A', 'Z'));
+        $pieces = str_split($data);
+        $temp = 0;
+        for ($i = 0; $i < count($pieces); $i++) {
+            //异或恢复
+            $pieces [$i] = ord($pieces [$i]);
+            $pieces [$i] = $pieces [$i] ^ ord($key [$temp]);
+
+            if ((strlen($key) - 1) <= $temp) {
+                $temp = 0;
+            } else {
+                $temp++;
+            }
+            //偏移恢复
+            $pieces [$i] = chr($pieces [$i] - 1);
+            //字符恢复
+            $pieces[$i] = isset($arr[$pieces[$i]]) ? $arr[$pieces[$i]] : $pieces[$i];
+        }
+        $result = base64_decode(join('', $pieces));
+        if ($checkdate) {
+            if (substr($result, 0, 10) < $nowtime || substr($result, 0, 10) != $time) {
+                throw new \Exception('The secret key timeout');
+            }
+        }
+
+        return str_replace($time, '', $result);
+    }
 }
